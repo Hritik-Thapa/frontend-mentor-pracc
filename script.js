21e5c2d9b1a831ae2734f51c7bfd086d73e22d68
Hritik-Thapa <hritikthapa331@gmail.com>
2024-01-14 15:58:09 +0545

    calculator functiosn

--- script.js
+++ script.js
@@ -32,22 +32,129 @@ document
 
 //calculator keys
 
+let result = 0;
+let display = false;
 document.querySelectorAll(`.calculator_keys button`).forEach((element) => {
-    const value=element.innerHTML;
-    const className=element.classList;
-    let screen=document.querySelector('.calculator_screen');
-    element.addEventListener("click", () => {
-        switch (className[0]){
-            case "keys_num":
-                console.log('number')
-                break;
-            case "keys_operator":
-                console.log('operator')
-                break;
-            default:
-                console.log('action')
-                break;
+  const value = element.innerHTML;
+  const className = element.classList;
+  const operators = ["+", "-", "/", "x"];
+
+  let input = document.querySelector(".calculator_screen span");
+  // let input=screen.innerHTML;
+  let lastCharacter;
+
+  element.addEventListener("click", () => {
+    lastCharacter = input.innerHTML[input.innerHTML.length - 1];
+    switch (className[0]) {
+      case "keys_num":
+        if (display === false) {
+          input.innerHTML = input.innerHTML + value;
+        } else {
+          input.innerHTML = "";
+          display = false;
+          input.innerHTML = input.innerHTML + value;
         }
-        screen.innerHTML=screen.innerHTML+value
+        break;
+
+      case "keys_operator":
+        if (value === "=") {
+          calc();
+        } else if (display == true) {
+          operatorClick(value);
+          display = false;
+        } else {
+          operatorClick(value);
+        }
+        break;
+
+      default:
+        if(value==="RESET" || display===true){
+          input.innerHTML='';
+        }
+        else{
+          input.innerHTML=input.innerHTML.slice(0,-1)
+        }
+        break;
+    }
   });
+
+  function operatorClick(operator) {
+    console.log(lastCharacter);
+    if (operators.includes(lastCharacter)) {
+      console.log("yo");
+      console.log(lastCharacter, operator);
+      input.innerHTML = input.innerHTML.slice(0, -1) + operator;
+    } else {
+      input.innerHTML += operator;
+    }
+  }
+
+  function calc() {
+    if (operators.includes(lastCharacter)) {
+      alert("end with a number");
+    } else {
+      let termsString = input.innerHTML.split(/\+|\-|\/|[x]/g);
+      let eqnOperator = input.innerHTML.replace(/[0-9]|\./g, "").split("");
+
+      terms=termsString.map((e) => Number(e));
+      console.log(terms,eqnOperator)
+      while (eqnOperator.includes("/")) {
+        const index = eqnOperator.indexOf("/");
+        terms.splice(index, 2, terms[index] / terms[index + 1]);
+        eqnOperator.splice(index, 1);
+      }
+      while (eqnOperator.includes("x")) {
+        const index = eqnOperator.indexOf("*");
+        terms.splice(index, 2, terms[index] * terms[index + 1]);
+        eqnOperator.splice(index, 1);
+      }
+      while (eqnOperator.includes("+")) {
+        const index = eqnOperator.indexOf("+");
+        terms.splice(index, 2, terms[index] + terms[index + 1]);
+        eqnOperator.splice(index, 1);
+      }
+      while (eqnOperator.includes("-")) {
+        const index = eqnOperator.indexOf("-");
+        terms.splice(index, 2, terms[index] - terms[index + 1]);
+        eqnOperator.splice(index, 1);
+      }
+
+      input.innerHTML=terms[0];
+      display=true
+      console.log(terms, eqnOperator);
+    }
+  }
+
+  // function operatorClick(screen,operator){
+  //   console.log(result,screen)
+  //   switch(operator){
+  //     case null:
+  //       result+=screen;
+  //       break;
+  //     case '+':
+  //       result+=screen;
+  //       break;
+  //     case '-':
+  //       result+=screen;
+  //       break;
+  //     case 'x':
+  //       result+=screen;
+  //       break;
+  //     case '/':
+  //       result+=screen;
+  //       break;
+  //     default:
+  //       equation=input.replace('x','*');
+  //       console.log(equation);
+  //       break;
+  //   }
+  //   // input=result;
+  //   console.log(operator,screen)
+  // }
+
+  // function actionClick(action){
+  //   action==='RESET'?result=0
+  // }
 });
+
+function numberClick(value) {}
